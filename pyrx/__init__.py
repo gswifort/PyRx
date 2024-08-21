df30855bda1b644dff0007377049641c1d707955
Grzegorz Świtek <grzegorz.switek@fort.pl>
2024-08-21 07:14:26 +0200

    add pyrx/__init__.py

--- pyrx/__init__.py
+++ pyrx/__init__.py
@@ -1,13 +1,39 @@
 from typing import TYPE_CHECKING
+import warnings
 
-import PyRx as Rx  # isort: skip  # type: ignore
-import PyGe as Ge  # isort: skip  # type: ignore
-import PyGi as Gi  # isort: skip  # type: ignore
-import PyDb as Db  # isort: skip  # type: ignore
-import PyAp as Ap  # isort: skip  # type: ignore
-import PyEd as Ed  # isort: skip  # type: ignore
-import PyPl as Pl  # isort: skip  # type: ignore
-import PyGs as Gs  # isort: skip  # type: ignore
+
+class PyRxModule:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __getattr__(self, name):
+        raise ModuleNotFoundError(
+            f"The {self.name!r} module is not available, it must be "
+            "invoked from a CAD application."
+        )
+
+
+try:
+    import PyRx as Rx  # isort: skip  # type: ignore
+    import PyGe as Ge  # isort: skip  # type: ignore
+    import PyGi as Gi  # isort: skip  # type: ignore
+    import PyDb as Db  # isort: skip  # type: ignore
+    import PyAp as Ap  # isort: skip  # type: ignore
+    import PyEd as Ed  # isort: skip  # type: ignore
+    import PyPl as Pl  # isort: skip  # type: ignore
+    import PyGs as Gs  # isort: skip  # type: ignore
+except ModuleNotFoundError:
+    warnings.warn(
+        "PyRx modules are not available, they must be invoked from a CAD application."
+    )
+    Rx = PyRxModule("PyRx")
+    Ge = PyRxModule("PyGe")
+    Gi = PyRxModule("PyGi")
+    Db = PyRxModule("PyDb")
+    Ap = PyRxModule("PyAp")
+    Ed = PyRxModule("PyEd")
+    Pl = PyRxModule("PyPl")
+    Gs = PyRxModule("PyGs")
 
 if TYPE_CHECKING:
     from .PyRxStubs import PyAp as Ap  # noqa: F811  # type: ignore
