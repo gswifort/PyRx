e299cec16289d58c8983df4e6055497448bb7ac6
Grzegorz Świtek <grzegorz.switek@fort.pl>
2024-09-06 13:19:31 +0200

    add get_layouts docstring

--- pyrx/db/layout.py
+++ pyrx/db/layout.py
@@ -7,6 +7,14 @@ from pyrx.db.misc import get_db
 
 
 def get_layouts(db: t.Optional[Db.Database] = None, model=False) -> t.Generator[Db.Layout, None, None]:
+    """
+    Retrieve layouts from the specified database, optionally excluding model space layouts.
+
+    Args:
+        db: The database to retrieve layouts from. Defaults to ``None``, in which case the default (working) database is used.
+        model: If ``False``, exclude the layout associated with model space. Defaults to ``False``.
+    """  # noqa: E501
+
     if db is None:
         db = get_db()
     ld = Db.Dictionary(db.layoutDictionaryId())
